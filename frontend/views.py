7e7c42672f4069744a33d9e14cc2c051f54aeac7
kramstyles <mark.eke@decagon.dev>
2022-04-03 15:02:01 +0100

    added frontend to installed apps

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class HomeView(generic.ListView):
+    model = None
+    template_name = 'frontend/index.html'
+
+
+def home_view(request):
+    context = {
+        'title': 'home'
+    }
+    return render(request, 'frontend/index.html', context)
