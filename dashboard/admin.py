824ee665927405669b7e613b3532872e2b1c266b
kramstyles <mark.eke@decagon.dev>
2022-04-04 21:44:09 +0100

    completed admin and models code

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(User)
+admin.site.register(Receptionist)
+admin.site.register(Room)
+admin.site.register(RoomStatus)
+admin.site.register(RoomType)
+admin.site.register(Booking)
+admin.site.register(Payment)
+admin.site.register(PaymentType)
+admin.site.register(Reservation)
