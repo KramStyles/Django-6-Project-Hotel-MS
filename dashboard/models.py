cf2890caacfd1b2100d1561823ca50e91a9b8025
IFEANYI OMEATA <omeatai@gmail.com>
2022-04-06 14:50:10 +0100

    updated models.py 64_to_240
    updated models.py 64_to_240

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=200, unique=True)
-    email = models.EmailField(max_length=200, unique=True)
-    phone_number = models.CharField(max_length=200, default=None, null=True)
-    password = models.CharField(max_length=200)
-    otp_code = models.CharField(max_length=200, default=None, null=True)
+    username = models.CharField(max_length=240, unique=True)
+    email = models.EmailField(max_length=240, unique=True)
+    phone_number = models.CharField(max_length=240, default=None, null=True)
+    password = models.CharField(max_length=240)
+    otp_code = models.CharField(max_length=240, default=None, null=True)
     email_verified = models.BooleanField(default=False)
     is_admin = models.BooleanField('Is Admin', default=False)
     is_superadmin = models.BooleanField('Is Super Admin', default=False)
@@ -20,10 +20,10 @@ class User(AbstractUser):
 
 class Receptionist(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200, unique=True)
-    last_name = models.CharField(max_length=200, unique=True)
-    gender = models.CharField(max_length=200)
-    avatar_url = models.CharField(max_length=200, null=True)
+    first_name = models.CharField(max_length=240, unique=True)
+    last_name = models.CharField(max_length=240, unique=True)
+    gender = models.CharField(max_length=240)
+    avatar_url = models.CharField(max_length=240, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
@@ -34,7 +34,7 @@ class Receptionist(models.Model):
 class Room(models.Model):
     room_type_id = models.ForeignKey('RoomType', on_delete=models.CASCADE)
     room_status_id = models.ForeignKey('RoomStatus', on_delete=models.CASCADE)
-    room_no = models.CharField(max_length=200, unique=True)
+    room_no = models.CharField(max_length=240, unique=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
@@ -44,7 +44,7 @@ class Room(models.Model):
 
 
 class RoomStatus(models.Model):
-    status = models.CharField(max_length=200, unique=True)
+    status = models.CharField(max_length=240, unique=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
@@ -53,8 +53,8 @@ class RoomStatus(models.Model):
 
 
 class RoomType(models.Model):
-    room_type = models.CharField(max_length=200, unique=True)
-    price = models.CharField(max_length=200)
+    room_type = models.CharField(max_length=240, unique=True)
+    price = models.CharField(max_length=240)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
@@ -87,7 +87,7 @@ class Payment(models.Model):
 
 
 class PaymentType(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=240, unique=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
